e5ee3a9670ef6522aff24d18adfe1c49dd60b6c4
dirhamtriyadi <dirhamtriyadi@gmail.com>
2024-11-24 16:09:47 +0700

    refactor: move form class to the end in blade templates
    This commit moves the action form class to the end of the blade templates. The class was previously inline with the button, which made it difficult to style. By moving the class to the end, it is now easier to style and does not affect the functionality of the form.

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -60,7 +60,7 @@
                                                     <td>
                                                         <div class="d-flex justify-content-center">
                                                             <a href="{{ route('roles.edit', $value->id) }}" class="btn btn-sm btn-warning"><i class="fas fa-edit"></i> Edit</a>
-                                                            <form action="{{ route('roles.destroy', $value->id) }}" method="post">
+                                                            <form action="{{ route('roles.destroy', $value->id) }}" class="ml-1" method="post">
                                                                 @csrf
                                                                 @method('delete')
                                                                 <button type="submit" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i> Delete</button>
--- resources/views/work-schedules/index.blade.php
+++ resources/views/work-schedules/index.blade.php
@@ -62,7 +62,7 @@
                                                     <td>
                                                         <div class="d-flex justify-content-center">
                                                             <a href="{{ route('work-schedules.edit', $value->id) }}" class="btn btn-sm btn-warning"><i class="fas fa-edit"></i> Edit</a>
-                                                            {{-- <form action="{{ route('work-schedules.destroy', $value->id) }}" method="post">
+                                                            {{-- <form action="{{ route('work-schedules.destroy', $value->id) }}" class="ml-1" method="post">
                                                                 @csrf
                                                                 @method('delete')
                                                                 <button type="submit" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i> Delete</button>
