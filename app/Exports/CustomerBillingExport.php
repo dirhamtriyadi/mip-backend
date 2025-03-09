bb2aa93582163d06724368f9752bfa1a64cdff65
dirhamtriyadi <dirhamtriyadi@gmail.com>
2025-03-09 10:03:35 +0700

    feat(export): add role-based filtering to billing export

--- app/Exports/CustomerBillingExport.php
+++ app/Exports/CustomerBillingExport.php
@@ -3,37 +3,47 @@
 namespace App\Exports;
 
 use App\Models\CustomerBilling;
-use Maatwebsite\Excel\Concerns\FromView;
 use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Illuminate\Support\Facades\Auth;
 
 class CustomerBillingExport implements FromView
 {
+    protected $start_date;
+    protected $end_date;
+    protected $user;
+
     public function __construct($start_date, $end_date)
     {
         $this->start_date = $start_date;
         $this->end_date = $end_date;
+        $this->user = Auth::user(); // Ambil data user yang sedang login
     }
-
-    // public function startDate($start_date)
-    // {
-    //     $this->start_date = $start_date;
-
-    //     return $this;
-    // }
-
-    // public function endDate($end_date)
-    // {
-    //     $this->end_date = $end_date;
-
-    //     return $this;
-    // }
 
     public function view(): View
     {
-        $billings = CustomerBilling::with(['customer', 'user'])->whereBetween('created_at', [$this->start_date, $this->end_date])->get();
+        // Query dasar dengan tanggal
+        $customerBilling = CustomerBilling::with(['customer', 'user', 'latestBillingFollowups'])
+            ->whereBetween('created_at', [$this->start_date, $this->end_date]);
+
+        // Terapkan filter berdasarkan izin pengguna
+        if (!$this->user->hasPermissionTo('laporan-penagihan.all-data')) {
+            if ($this->user->hasPermissionTo('laporan-penagihan.data-my-bank')) {
+                // Filter berdasarkan bank_id
+                $customerBilling->whereHas('customer', function ($query) {
+                    $query->where('bank_id', $this->user->bank_id);
+                });
+            } else {
+                // Jika tidak punya izin melihat semua data atau data bank, hanya tampilkan data miliknya
+                $customerBilling->where('user_id', $this->user->id);
+            }
+        }
+
+        // Eksekusi query setelah semua filter diterapkan
+        $customerBillings = $customerBilling->get();
 
         return view('customer-billing-reports.excel', [
-            'data' => $billings
+            'data' => $customerBillings
         ]);
     }
 }
