246028f5ff0187f50ef81a9e61a33edcfe0a09ba
dirhamtriyadi <dirhamtriyadi@gmail.com>
2025-03-10 21:01:52 +0700

    fix(templates): improve title display logic

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name') }} | {{ Route::currentRouteName() }}</title>
+    <title>{{ config('app.name') }} | {{ Route::currentRouteName() ? Str::replace('-', ' ', Str::title(Str::before(Route::currentRouteName(), '.'))) : '' }}</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
