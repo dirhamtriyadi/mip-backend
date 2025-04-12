fae0d8471e3446207caaaf571d25cdfffd7bf770
dirhamtriyadi <dirhamtriyadi@gmail.com>
2025-04-12 11:32:47 +0700

    feat(customer): allow null status for prospective customer

--- resources/views/prospective-customers/action.blade.php
+++ resources/views/prospective-customers/action.blade.php
@@ -1,8 +1,10 @@
 <div class="d-flex justify-content-center">
-    <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#modal-proccess-customer"
-        data-prospectiveCustomer="{{ json_encode($value) }}">
-        <i class="fas fa-spinner"></i> Proses
-    </button>
+    @if ($value->status === 'pending')
+        <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#modal-proccess-customer"
+            data-prospectiveCustomer="{{ json_encode($value) }}">
+            <i class="fas fa-spinner"></i> Proses
+        </button>
+    @endif
     <a href="{{ route('prospective-customers.edit', $value->id) }}" class="btn btn-sm btn-warning ml-1"><i
             class="fas fa-edit"></i> Edit</a>
     <form action="{{ route('prospective-customers.destroy', $value->id) }}" class="ml-1" method="post">
