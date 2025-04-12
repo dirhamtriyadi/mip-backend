b6d8d89f7de82d833466ac44c33f8fb0f6d8b7f5
dirhamtriyadi <dirhamtriyadi@gmail.com>
2025-04-12 12:06:53 +0700

    style(pdf): improve pdf styling and layout

--- resources/views/prospective-customer-surveys/pdf-by-customer.blade.php
+++ resources/views/prospective-customer-surveys/pdf-by-customer.blade.php
@@ -34,6 +34,12 @@
             display: inline-block;
         }
 
+        .value1 {
+            width: 90%;
+            border-bottom: 1px solid #000;
+            display: inline-block;
+        }
+
         /* Agar tabel menyesuaikan isi */
         table {
             width: 100%;
@@ -43,6 +49,14 @@
 
         th,
         td {
+            text-align: left;
+            padding: 5px;
+            /* border: 1px solid #ddd; */
+            font-size: 11px;
+        }
+
+        .table-file th,
+        .table-file td {
             text-align: left;
             padding: 5px;
             border: 1px solid #ddd;
@@ -86,235 +100,231 @@
     </table>
 
     <table>
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Fitur</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><strong>1</strong></td>
-                <td><strong>CIF</strong></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Nama</td>
-                <td><span class="value">: {{ $data->name }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Alamat</td>
-                <td><span class="value">: {{ $data->address }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>No. KTP</td>
-                <td><span class="value">: {{ $data->number_ktp }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Status Alamat</td>
-                <td><span class="value">: {{ $data->address_status }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>No. Telp</td>
-                <td><span class="value">: {{ $data->phone_number }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>NPWP</td>
-                <td><span class="value">: {{ $data->npwp }}</span></td>
-            </tr>
-            <tr>
-                <td><strong>2</strong></td>
-                <td><strong>Pendapatan</strong></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Jenis Pekerjaan</td>
-                <td><span class="value">: {{ $data->job_type }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Nama Perusahaan</td>
-                <td><span class="value">: {{ $data->company_name }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Jabatan</td>
-                <td><span class="value">: {{ $data->job_level }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Lama Kerja</td>
-                <td><span class="value">: {{ $data->employee_tenure }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Status Karyawan</td>
-                <td><span class="value">: {{ $data->employee_status }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Gaji</td>
-                <td><span class="value">: {{ $data->salary }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Usaha Tambahan</td>
-                <td><span class="value">: {{ $data->other_business }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Biaya hidup per bulan</td>
-                <td><span class="value">: {{ $data->monthly_living_expenses }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><strong>Tanggungan</strong></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Anak</td>
-                <td><span class="value">: {{ $data->children }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Istri</td>
-                <td><span class="value">: {{ $data->wife }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><strong>Pendapatan Pasangan</strong></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Pekerjaan Pasangan</td>
-                <td><span class="value">: {{ $data->couple_jobs }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Usaha Pasangan</td>
-                <td><span class="value">: {{ $data->couple_business }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Pendapatan Pasangan</td>
-                <td><span class="value">: {{ $data->couple_income }}</span></td>
-            </tr>
-            <tr>
-                <td><strong>3</strong></td>
-                <td><strong>Hutang</strong></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Bank</td>
-                <td><span class="value">: {{ $data->bank_debt }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Koperasi</td>
-                <td><span class="value">: {{ $data->cooperative_debt }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Perorangan</td>
-                <td><span class="value">: {{ $data->personal_debt }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Online</td>
-                <td><span class="value">: {{ $data->online_debt }}</span></td>
-            </tr>
-            <tr>
-                <td>4</td>
-                <td>Scorring</td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Analisa Karakter Nasabah</td>
-                <td><span class="value">: {{ $data->customer_character_analysis }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Analisa Laporan Keuangan</td>
-                <td><span class="value">: {{ $data->financial_report_analysis }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Hasil Slik</td>
-                <td><span class="value">: {{ $data->slik_result }}</span></td>
-            </tr>
-            <tr>
-                <td>5</td>
-                <td>Informasi Tambahan dan Pengajuan</td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Nama Pemberi Informasi</td>
-                <td><span class="value">: {{ $data->info_provider_name }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Jabatan Pemberi Informasi</td>
-                <td><span class="value">: {{ $data->info_provider_position }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Kondisi Tempat Kerja</td>
-                <td><span class="value">: {{ $data->workplace_condition }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Banyak Karyawan</td>
-                <td><span class="value">: {{ $data->employee_count }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Lama Usaha Kantor</td>
-                <td><span class="value">: {{ $data->business_duration }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Alamat Kantor</td>
-                <td><span class="value">: {{ $data->office_address }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Telepon Kantor</td>
-                <td><span class="value">: {{ $data->office_phone }}</span></td>
-            </tr>
-            <tr>
-                <td>6</td>
-                <td>Rekomendasi dari</td>
-                <td></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Vendor</td>
-                <td><span class="value">: {{ $data->recommendation_from_vendors }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Bendahara</td>
-                <td><span class="value">: {{ $data->recommendation_from_treasurer }}</span></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>Lainnya</td>
-                <td><span class="value">: {{ $data->recommendation_from_other }}</span></td>
-            </tr>
-        </tbody>
+        <tr>
+            <th>No</th>
+            <th>Fitur</th>
+            <th></th>
+        </tr>
+        <tr>
+            <td><strong>1</strong></td>
+            <td><strong>CIF</strong></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Nama</td>
+            <td><span class="value">: {{ $data->name }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Alamat</td>
+            <td><span class="value">: {{ $data->address }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>No. KTP</td>
+            <td><span class="value">: {{ $data->number_ktp }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Status Alamat</td>
+            <td><span class="value">: {{ $data->address_status }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>No. Telp</td>
+            <td><span class="value">: {{ $data->phone_number }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>NPWP</td>
+            <td><span class="value">: {{ $data->npwp }}</span></td>
+        </tr>
+        <tr>
+            <td><strong>2</strong></td>
+            <td><strong>Pendapatan</strong></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Jenis Pekerjaan</td>
+            <td><span class="value">: {{ $data->job_type }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Nama Perusahaan</td>
+            <td><span class="value">: {{ $data->company_name }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Jabatan</td>
+            <td><span class="value">: {{ $data->job_level }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Lama Kerja</td>
+            <td><span class="value">: {{ $data->employee_tenure }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Status Karyawan</td>
+            <td><span class="value">: {{ $data->employee_status }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Gaji</td>
+            <td><span class="value">: {{ $data->salary }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Usaha Tambahan</td>
+            <td><span class="value">: {{ $data->other_business }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Biaya hidup per bulan</td>
+            <td><span class="value">: {{ $data->monthly_living_expenses }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><strong>Tanggungan</strong></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Anak</td>
+            <td><span class="value">: {{ $data->children }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Istri</td>
+            <td><span class="value">: {{ $data->wife }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><strong>Pendapatan Pasangan</strong></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Pekerjaan Pasangan</td>
+            <td><span class="value">: {{ $data->couple_jobs }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Usaha Pasangan</td>
+            <td><span class="value">: {{ $data->couple_business }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Pendapatan Pasangan</td>
+            <td><span class="value">: {{ $data->couple_income }}</span></td>
+        </tr>
+        <tr>
+            <td><strong>3</strong></td>
+            <td><strong>Hutang</strong></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Bank</td>
+            <td><span class="value">: {{ $data->bank_debt }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Koperasi</td>
+            <td><span class="value">: {{ $data->cooperative_debt }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Perorangan</td>
+            <td><span class="value">: {{ $data->personal_debt }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Online</td>
+            <td><span class="value">: {{ $data->online_debt }}</span></td>
+        </tr>
+        <tr>
+            <td><strong>4</strong></td>
+            <td><strong>Scorring</strong></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Analisa Karakter Nasabah</td>
+            <td><span class="value">: {{ $data->customer_character_analysis }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Analisa Laporan Keuangan</td>
+            <td><span class="value">: {{ $data->financial_report_analysis }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Hasil Slik</td>
+            <td><span class="value">: {{ $data->slik_result }}</span></td>
+        </tr>
+        <tr>
+            <td><strong>5</strong></td>
+            <td><strong>Informasi Tambahan dan Pengajuan</strong></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Nama Pemberi Informasi</td>
+            <td><span class="value">: {{ $data->info_provider_name }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Jabatan Pemberi Informasi</td>
+            <td><span class="value">: {{ $data->info_provider_position }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Kondisi Tempat Kerja</td>
+            <td><span class="value">: {{ $data->workplace_condition }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Banyak Karyawan</td>
+            <td><span class="value">: {{ $data->employee_count }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Lama Usaha Kantor</td>
+            <td><span class="value">: {{ $data->business_duration }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Alamat Kantor</td>
+            <td><span class="value">: {{ $data->office_address }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Telepon Kantor</td>
+            <td><span class="value">: {{ $data->office_phone }}</span></td>
+        </tr>
+        <tr>
+            <td><strong>6</strong></td>
+            <td><strong>Rekomendasi dari</strong></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Vendor</td>
+            <td><span class="value">: {{ $data->recommendation_from_vendors }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Bendahara</td>
+            <td><span class="value">: {{ $data->recommendation_from_treasurer }}</span></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>Lainnya</td>
+            <td><span class="value">: {{ $data->recommendation_from_other }}</span></td>
+        </tr>
     </table>
     <table class="wawancara-container">
         <tbody>
@@ -424,8 +434,8 @@
     </table>
     <table>
         <tr>
-            <td>9</td>
-            <td>Catatan Rekomendasi PT</td>
+            <td><strong>9</strong></td>
+            <td><strong>Catatan Rekomendasi PT</strong></td>
             <td></td>
         </tr>
         <tr>
@@ -452,42 +462,48 @@
             <td><span class="value">: {{ $data->longitude ?? '0' }}</span></td>
         </tr>
     </table>
-    <table style="width: 100%; border-collapse: collapse; table-layout: fixed;">
+    <table style="width: 100%; border-collapse: collapse; table-layout: fixed; margin-top: 50px">
         <tr style="width: 33.33%;">
-            <td>Petugas</td>
-            <td>Nasabah</td>
-            <td>Pasangan/Penanggung Jawab</td>
+            <td style="text-align: center"><strong>Petugas</strong></td>
+            <td style="text-align: center"><strong>Nasabah</strong></td>
+            <td style="text-align: center"><strong>Pasangan/Penanggung Jawab</strong></td>
         </tr>
         <tr>
             @if (!empty($data->signature_officer))
-                <td style="width: 33.33%; height: 200px;">
+                <td style="width: 33.33%; height: 150px;">
                     <img src="{{ public_path('storage/' . $data->signature_officer) }}" alt="TTD Petugas"
                         style="width: 100px; height: 100px;">
                 </td>
             @else
-                <td style="width: 33.33%; height: 200px;"></td>
+                <td style="width: 33.33%; height: 150px;"></td>
             @endif
 
             @if (!empty($data->signature_customer))
-                <td style="width: 33.33%; height: 200px;">
+                <td style="width: 33.33%; height: 150px;">
                     <img src="{{ public_path('storage/' . $data->signature_customer) }}" alt="TTD Customer"
                         style="width: 100px; height: 100px;">
                 </td>
             @else
-                <td style="width: 33.33%; height: 200px;"></td>
+                <td style="width: 33.33%; height: 150px;"></td>
             @endif
 
             @if (!empty($data->signature_couple))
-                <td style="width: 33.33%; height: 200px;">
+                <td style="width: 33.33%; height: 150px;">
                     <img src="{{ public_path('storage/' . $data->signature_couple) }}" alt="TTD Couple"
                         style="width: 100px; height: 100px;">
                 </td>
             @else
-                <td style="width: 33.33%; height: 200px;"></td>
+                <td style="width: 33.33%; height: 150px;"></td>
             @endif
         </tr>
+        <tr>
+            <td style="height: 20px; text-align: center">( <span class="value1">{{ $data->user->name ?? '' }}</span> )
+            </td>
+            <td style="height: 20px; text-align: center">( <span class="value1">{{ $data->name ?? '' }}</span> )</td>
+            <td style="height: 20px; text-align: center">( <span class="value1"></span> )</td>
+        </tr>
     </table>
-    <table style="width: 100%; border-collapse: collapse; table-layout: fixed;">
+    <table class="table-file" style="width: 100%; border-collapse: collapse; table-layout: fixed;">
         <tr style="width: 33.33%;">
             <td style="width: 33.33%; height: 150px;">1. Latitude/Longitude dab Gedung</td>
             <td style="width: 33.33%; height: 150px;">
